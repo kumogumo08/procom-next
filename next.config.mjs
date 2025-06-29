15488aca0058a81d525ee3b70f64fca0f372fa24
kumogumo08 <ebisawat0821dance@eagle.ocn.ne.jp>
2025-06-29 15:04:50 +0900

    jsx:react-jsx,のビルドエラー対策

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    FIREBASE_KEY_PATH: process.env.FIREBASE_KEY_PATH,
+  },
   experimental: {
+    // 必要があればここに書く
   },
 };
 

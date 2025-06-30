24da7a46ac4bddb94792b3732fee509474c2d9bb
kumogumo08 <ebisawat0821dance@eagle.ocn.ne.jp>
2025-06-30 22:06:53 +0900

    Google Search Console

--- next-sitemap.config.cjs
+++ next-sitemap.config.cjs
@@ -1,11 +1,11 @@
-// next-sitemap.config.js
+// next-sitemap.config.cjs
 
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://procom-next.onrender.com', // ← ★あなたの本番URL
+  siteUrl: 'https://procom-next.onrender.com',
   generateRobotsTxt: true,
   sitemapSize: 7000,
   changefreq: 'weekly',
   priority: 0.7,
-  exclude: ['/account', '/login', '/register'], // 非公開ページがあれば指定
+  exclude: ['/account', '/login', '/register'],
 };

efa440fa7d87890487940deae35a70111f41f74e
kumogumo08 <ebisawat0821dance@eagle.ocn.ne.jp>
2025-07-05 16:25:23 +0900

    推しボタン導入

--- app/api/oshi/[uid]/route.ts
+++ app/api/oshi/[uid]/route.ts
@@ -1,4 +1,6 @@
 // app/api/oshi/[uid]/route.ts
+export const runtime = 'nodejs';
+
 import { NextRequest, NextResponse } from 'next/server';
 import { initializeFirebaseAdmin } from '@/lib/firebase';
 import { getFirestore } from 'firebase-admin/firestore';
